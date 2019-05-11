ab8ed4cd5b7357b0796f446079d8f322116f7193
BhavikJoshi <bhavik@getwalnut.com>
2019-05-12 00:57:07 +0530

    Developed Login Functionality

--- controllers/index.js
+++ controllers/index.js
@@ -33,7 +33,7 @@ exports.register = async (req, res) => {
     1000, // Number of Iterations
     64, // Key Length
     'sha512' // Digest
-  ).toString('hex');
+  ).toString('hex'); // Convert to Hex String
 
   // Try creating a DB entry for the user
   try {
@@ -59,7 +59,7 @@ exports.register = async (req, res) => {
   }
 }
 
-exports.verifyEmail = async (req, res) => {
+exports.verifyEmail = (req, res) => {
   // Check if Token is present
   if (!req.params.token) {
     return res.status(400).send('Bad Request! No token present');
@@ -80,4 +80,45 @@ exports.verifyEmail = async (req, res) => {
     // return error response
     return res.status(500).send('Error!');
   })
+}
+
+exports.signIn = async (req, res) => {
+  console.log('Data', req.body);
+  // Check if email and password are provided
+  if(!req.body.email) {
+    return res.status(500).send('Please provide an Email');
+  }
+  if(!req.body.password) {
+    return res.status(500).send('Please provide a Password');
+  }
+  // Find User with provided email in database.
+  const user = await User.findOne({ email: req.body.email })
+  
+  if (!user) {
+    // No User found with provided email
+    return res.status(406).send('No account found with provided email address.');
+  }
+  
+  if (!user.isValidated) {
+    // User has not validated the email yet.
+    return res.status(403).send('Cannot Login! Please verify your email first.');
+  }
+
+  // Hash password with sha512 digest
+  let hashedPassword = crypto.pbkdf2Sync(
+    req.body.password, // Plain text password
+    user.token, // Using token as a salt
+    1000, // Number of Iterations
+    64, // Key Length
+    'sha512' // Digest
+  ).toString('hex'); // Convert to Hex String
+
+  // Check if hash matches database records
+  if (user.password === hashedPassword) {
+    // Login Successful
+    return res.status(202).send(`User ${user.email} has successfully logged in!`);
+  } else {
+    // Login Unsuccessful. Password and Email doesnot match records!
+    return res.status(401).send(`Unauthorized. Email and Password combination does not match our records!`);
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const controller = require('../controllers/index');
 router.get('/', controller.hello); // Hello route for checking
 router.post('/register', controller.register); // Register route for user sign up
 router.get('/verify/:token', controller.verifyEmail); // Verify route for verifying user emails
+router.post('/login', controller.signIn); // Sign In route for logging user in.
 
 // Export Routes
 module.exports = router;
